d9f2191fa8ddff43802aea58688ca76f58f17c18
Jorge-Pomo <50466842+Jorge-Pomo@users.noreply.github.com>
2021-05-11 08:13:54 +0200

    Add files via upload

--- Windowbuilder/src/T10_ej4/Logger.java
+++ Windowbuilder/src/T10_ej4/Logger.java
@@ -9,6 +9,7 @@ import javax.swing.border.EmptyBorder;
 import javax.swing.GroupLayout;
 import javax.swing.GroupLayout.Alignment;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JButton;
 import javax.swing.JTextField;
 import javax.swing.LayoutStyle.ComponentPlacement;
@@ -19,6 +20,13 @@ import javax.swing.JPasswordField;
 import javax.swing.JDesktopPane;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Logger extends JFrame {
 
@@ -35,10 +43,12 @@ public class Logger extends JFrame {
 				try {
 					Logger frame = new Logger();
 					frame.setVisible(true);
+
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
 			}
+
 		});
 	}
 
@@ -46,91 +56,114 @@ public class Logger extends JFrame {
 	 * Create the frame.
 	 */
 	public Logger() {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		addWindowListener(new WindowAdapter() {
+			@Override
+			public void windowClosing(WindowEvent arg0) {
+				JOptionPane cerrarVentana = new JOptionPane();
+				int opc = cerrarVentana.showConfirmDialog(null, "¿Quieres cerrar la entana?", "Cerrar ventana",
+						JOptionPane.YES_NO_CANCEL_OPTION);
+
+				if (opc == JOptionPane.YES_OPTION) {
+					System.exit(0);
+				}
+			}
+		});
+		setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
-		
+
 		JLabel lblUser = new JLabel("user:");
-		
+
 		JLabel lblPassword = new JLabel("password:");
-		
+
 		txtUser = new JTextField();
 		txtUser.setColumns(10);
-		
+
 		passwordField = new JPasswordField();
-		
+
 		String contra = "admin";
 		char[] correctPasswrd = contra.toCharArray();
 		passwordField.setEchoChar('*');
+
+		// Boton Login
 		JButton btnLogin = new JButton("LOGIN");
 		btnLogin.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				if(txtUser.getText().equals("admin") && Arrays.equals(passwordField.getPassword(), correctPasswrd)) {
-					Ok ventana = new Ok();
+
+				try {
+					Class.forName("com.mysql.jdbc.Driver");
+					Connection conexion = DriverManager.getConnection("jdbc:mysql://localhost/b_login", "root", "");
+
+					Statement s = conexion.createStatement();
+					ResultSet rs = s.executeQuery("select * from log where usuario='"+txtUser.getText()+"' AND Contraseña='"+passwordField.getPassword()+"';");
 				
-					ventana.setVisible(true);
-				}else {
-					error ventanaE = new error();
-					
-					ventanaE.setVisible(true);
+					txtUser.setText("SI");
+				} catch (ClassNotFoundException | SQLException e1) {
+					e1.printStackTrace();
 				}
-			
+				
+
+				
+				
+				
+				
+				
+				
+				
+				// if(txtUser.getText().equals("admin") &&
+				// Arrays.equals(passwordField.getPassword(), correctPasswrd)) {
+				// Ok ventana = new Ok();
+				//
+				// ventana.setVisible(true);
+				// }else {
+				// error ventanaE = new error();
+				//
+				// ventanaE.setVisible(true);
+				// }
+
 			}
 		});
-		
+
 		JButton btnMostrar = new JButton("Mostrar");
 		btnMostrar.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mousePressed(MouseEvent arg0) {
-				passwordField.setEchoChar((char)0);
+				passwordField.setEchoChar((char) 0);
 			}
+
 			@Override
 			public void mouseReleased(MouseEvent e) {
 				passwordField.setEchoChar('*');
 			}
 		});
-		
+
 		GroupLayout gl_contentPane = new GroupLayout(contentPane);
-		gl_contentPane.setHorizontalGroup(
-			gl_contentPane.createParallelGroup(Alignment.LEADING)
-				.addGroup(gl_contentPane.createSequentialGroup()
-					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
-						.addGroup(gl_contentPane.createSequentialGroup()
-							.addGap(24)
-							.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
-								.addGroup(gl_contentPane.createSequentialGroup()
-									.addComponent(lblPassword)
-									.addPreferredGap(ComponentPlacement.UNRELATED)
-									.addComponent(passwordField, 130, 130, 130)
-									.addPreferredGap(ComponentPlacement.RELATED)
-									.addComponent(btnMostrar))
-								.addGroup(gl_contentPane.createSequentialGroup()
-									.addComponent(lblUser)
-									.addGap(39)
-									.addComponent(txtUser, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))))
-						.addGroup(gl_contentPane.createSequentialGroup()
-							.addGap(151)
-							.addComponent(btnLogin)))
-					.addContainerGap(75, Short.MAX_VALUE))
-		);
-		gl_contentPane.setVerticalGroup(
-			gl_contentPane.createParallelGroup(Alignment.LEADING)
-				.addGroup(gl_contentPane.createSequentialGroup()
-					.addGap(26)
-					.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
-						.addComponent(lblUser)
-						.addComponent(txtUser, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
-					.addGap(31)
-					.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
-						.addComponent(lblPassword)
-						.addComponent(passwordField, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+		gl_contentPane.setHorizontalGroup(gl_contentPane.createParallelGroup(Alignment.LEADING).addGroup(gl_contentPane
+				.createSequentialGroup()
+				.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+						.addGroup(gl_contentPane.createSequentialGroup().addGap(24)
+								.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+										.addGroup(gl_contentPane.createSequentialGroup().addComponent(lblPassword)
+												.addPreferredGap(ComponentPlacement.UNRELATED)
+												.addComponent(passwordField, 130, 130, 130)
+												.addPreferredGap(ComponentPlacement.RELATED).addComponent(btnMostrar))
+										.addGroup(gl_contentPane.createSequentialGroup().addComponent(lblUser)
+												.addGap(39).addComponent(txtUser, GroupLayout.PREFERRED_SIZE,
+														GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))))
+						.addGroup(gl_contentPane.createSequentialGroup().addGap(151).addComponent(btnLogin)))
+				.addContainerGap(75, Short.MAX_VALUE)));
+		gl_contentPane.setVerticalGroup(gl_contentPane.createParallelGroup(Alignment.LEADING).addGroup(gl_contentPane
+				.createSequentialGroup().addGap(26)
+				.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE).addComponent(lblUser).addComponent(
+						txtUser, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+				.addGap(31)
+				.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE).addComponent(lblPassword)
+						.addComponent(passwordField, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE,
+								GroupLayout.PREFERRED_SIZE)
 						.addComponent(btnMostrar))
-					.addGap(62)
-					.addComponent(btnLogin)
-					.addContainerGap(69, Short.MAX_VALUE))
-		);
+				.addGap(62).addComponent(btnLogin).addContainerGap(69, Short.MAX_VALUE)));
 		contentPane.setLayout(gl_contentPane);
 	}
 }
---
+++ Windowbuilder/src/T11_ej1/ConectarBBDD.java
@@ -0,0 +1,9 @@
+package T11_ej1;
+
+public class ConectarBBDD {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
